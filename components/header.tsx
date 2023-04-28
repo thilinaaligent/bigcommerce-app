2034bd9b60b5e0111ce77b6d2a0619f5a0062542
Thilina Gunasekara <thilina@aligent.com.au>
2023-04-28 10:11:14 +0930

    BCE-1: add locations api and list component

--- components/header.tsx
+++ components/header.tsx
@@ -1,37 +1,37 @@
-import { Box, Tabs } from '@bigcommerce/big-design';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import InnerHeader from './innerHeader';
+import { Box, Tabs } from "@bigcommerce/big-design";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import InnerHeader from "./innerHeader";
 
 export const TabIds = {
-    HOME: 'home',
-    PRODUCTS: 'products',
+    HOME: "home",
+    PRODUCTS: "products",
+    LOCATIONS: "locations",
 };
 
 export const TabRoutes = {
-    [TabIds.HOME]: '/',
-    [TabIds.PRODUCTS]: '/products',
+    [TabIds.HOME]: "/",
+    [TabIds.PRODUCTS]: "/products",
+    [TabIds.LOCATIONS]: "/locations",
 };
 
 const HeaderlessRoutes = [
-    '/orders/[orderId]',
-    '/orders/[orderId]/labels',
-    '/orders/[orderId]/modal',
-    '/productAppExtension/[productId]',
+    "/orders/[orderId]",
+    "/orders/[orderId]/labels",
+    "/orders/[orderId]/modal",
+    "/productAppExtension/[productId]",
 ];
 
-const InnerRoutes = [
-    '/products/[pid]',
-];
+const InnerRoutes = ["/products/[pid]"];
 
 const HeaderTypes = {
-    GLOBAL: 'global',
-    INNER: 'inner',
-    HEADERLESS: 'headerless',
+    GLOBAL: "global",
+    INNER: "inner",
+    HEADERLESS: "headerless",
 };
 
 const Header = () => {
-    const [activeTab, setActiveTab] = useState<string>('');
+    const [activeTab, setActiveTab] = useState<string>("");
     const [headerType, setHeaderType] = useState<string>(HeaderTypes.GLOBAL);
     const router = useRouter();
     const { pathname } = router;
@@ -44,23 +44,25 @@ const Header = () => {
             setHeaderType(HeaderTypes.HEADERLESS);
         } else {
             // Check if new route matches TabRoutes
-            const tabKey = Object.keys(TabRoutes).find(key => TabRoutes[key] === pathname);
+            const tabKey = Object.keys(TabRoutes).find(
+                (key) => TabRoutes[key] === pathname
+            );
 
             // Set the active tab to tabKey or set no active tab if route doesn't match (404)
-            setActiveTab(tabKey ?? '');
+            setActiveTab(tabKey ?? "");
             setHeaderType(HeaderTypes.GLOBAL);
         }
-
     }, [pathname]);
 
     useEffect(() => {
         // Prefetch products page to reduce latency (doesn't prefetch in dev)
-        router.prefetch('/products');
+        router.prefetch("/products");
     });
 
     const items = [
-        { ariaControls: 'home', id: TabIds.HOME, title: 'Home' },
-        { ariaControls: 'products', id: TabIds.PRODUCTS, title: 'Products' },
+        { ariaControls: "home", id: TabIds.HOME, title: "Home" },
+        { ariaControls: "products", id: TabIds.PRODUCTS, title: "Products" },
+        { ariaControls: "locations", id: TabIds.LOCATIONS, title: "Locations" },
     ];
 
     const handleTabClick = (tabId: string) => {
